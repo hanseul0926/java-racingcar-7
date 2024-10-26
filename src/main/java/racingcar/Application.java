77dad477900cbe8bf3060d5eb7fa95919bfac4cf
hanseul0926 <oneseol22@gmail.com>
2024-10-27 04:07:01 +0900

    feat:사용자 문자열 입력 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,24 @@
 package racingcar;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String str = scanner.nextLine();
+        String[] name = str.split(",");
+        if //이름이 5글자 이상이면 반환
+
+
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        System.out.println("실행 결과");
+        System.out.print("최종 우승자 : " + name);
+
     }
 }
+
